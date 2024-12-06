0782a714250548e7b0af3079e4ebe8ad87fc1061
Mahilson Pereira <m.hagner.sp@gmail.com>
2024-12-06 16:11:38 -0300

    next test jes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filesnames) => [
-    `npx prettier ${filesnames.join(' ')} --write`,
-    `npm run lint --fix . ${filesnames.join(' --file')}`
+    `npx prettier src/ --write`,
+    `npm run lint --fix . ${filesnames.join(' --file')}`,
+    `npm test -- --findRelatedTests ${filesnames.join(' ')}`
   ]
 }
